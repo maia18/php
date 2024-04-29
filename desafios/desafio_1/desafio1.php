b6ae6513536bb7d094df1afca1f240c6c759fdad
maia18 <nelsonkaue05100@gmail.com>
2024-04-29 15:57:59 -0300

    fim da aula de hoje :)

--- desafios/desafio_1/desafio1.php
+++ desafios/desafio_1/desafio1.php
@@ -1,3 +1,30 @@
+<!-- Antes -->
+
+<!-- <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1ºDesafio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado</h1>
+    </header>
+    <main>
+        // <//?php
+            // $num = $_GET['numero'];
+            // $num_su = $num + 1;
+            // $num_antec = $num - 1;
+            // echo "O sucessor e antecessor de <strong>$num</strong> são, respectivamente, <strong>$num_su</strong> e <strong>$num_antec</strong>";
+        // ?>
+        <p style="font-size: 10px;"><a href="Javascript:history.go(-1)">Retornar</a></p>
+    </main>
+</body>
+</html> -->
+
+<!-- Depois -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,11 +40,10 @@
     <main>
         <?php 
             $num = $_GET['numero'];
-            $num_su = $num + 1;
-            $num_antec = $num - 1;
-            echo "O sucessor e antecessor de <strong>$num</strong> são, respectivamente, <strong>$num_su</strong> e <strong>$num_antec</strong>";
+            echo "O sucessor e antecessor de <strong>$num</strong> são, respectivamente, <strong>".($num + 1)."</strong> e <strong>".($num - 1)."</strong>";
         ?>
-        <p style="font-size: 10px;"><a href="Javascript:history.go(-1)">Retornar</a></p>
+        <!-- <button onclick="javascript:window.location.href='desafio_1.html'">&#x2B05 Retornar</button> -->
+        <button onclick="Javascript:history.go(-1)">&#x2B05 Retornar</button>
     </main>
 </body>
 </html>
--- desafios/desafio_5/desafio_5.php
+++ desafios/desafio_5/desafio_5.php
@@ -1,3 +1,30 @@
+<!-- Antes -->
+
+<!-- <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Analisador de Números Reais</h1>
+    </header>
+    <main>
+        //</?php 
+            // $num = $_GET["numero"];
+            // $num_real = intval($num);
+            // $num_fraç = round($num - floor($num), 6);
+            // echo "Número digitado: <strong>$num</strong> <br>Parte Real: <strong>$num_real</strong></br> <br>Parte Fracionária: <strong>$num_fraç</strong> </br>"
+        ?>
+        <p style="font-size: 10px;"><a href="javascript:history.go(-1)">Retornar</a></p>
+    </main>
+</body>
+</html> -->
+
+<!-- Depois -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -12,10 +39,11 @@
     </header>
     <main>
         <?php 
-            $num = $_GET["numero"];
-            $num_real = intval($num);
-            $num_fraç = round($num - floor($num), 6);
-            echo "Número digitado: <strong>$num</strong> <br>Parte Real: <strong>$num_real</strong></br> <br>Parte Fracionária: <strong>$num_fraç</strong> </br>"
+            $num = $_REQUEST["numero"];
+            $num_real = (int) $num;
+            $num_fraç = $num - $num_real;
+            echo "Número digitado: <strong>$num</strong><ul><li>Parte Real: <strong>".(number_format($num_real, 0,",", "."))."</strong></li></ul><ul><li>Parte Fracionária: <strong>".(number_format($num_fraç, 
+            4, ",", "."))."</strong></li></ul>"
         ?>
         <p style="font-size: 10px;"><a href="javascript:history.go(-1)">Retornar</a></p>
     </main>
