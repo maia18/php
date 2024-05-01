d194fda83849503bf94b762605938231d389d897
maia18 <nelsonkaue05100@gmail.com>
2024-05-01 17:24:42 -0300

    .

--- desafios/desafio_10/desafio_10.php
+++ desafios/desafio_10/desafio_10.php
@@ -10,21 +10,20 @@
     <?php 
         $year_old = $_GET['year_old'] ?? 0;
         $year_des = $_GET['year_des'] ?? 0;
-        $year_att = date('Y');
     ?>
     <main>
         <form action="<?=$_SERVER['PHP_SELF']?>">
             <label for="year_old">Year of birth</label>
             <input type="number" name="year_old" id="year_old" pattern=".{4}">
-            <label for="year_des">Year that you wish know your age ( year actually: <?=$year_att?> )</label>
+            <label for="year_des">Year that you wish know your age ( year actually: <?=date('Y')?> )</label>
             <input type="number" name="year_des" id="year_des" pattern=".{4}">
             <input type="submit" value="Show Age">
         </form>
     </main>
     <section>
             <?php 
-                if($year_des == $year_att) {
-                    $age = $year_att - $year_old;
+                if($year_des == date('Y')) {
+                    $age = date('Y') - $year_old;
                     echo "Age: <strong>$age</strong>";
                 }else if($year_des < $year_old){
                     user_error("<strong>($year_des < $year_old)</strong>");
