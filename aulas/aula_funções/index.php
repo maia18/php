2251ae082ff390f84433037302dc94c043f8c8ba
maia18 <nelsonkaue05100@gmail.com>
2024-05-06 15:00:19 -0300

    Create index.php

---
+++ aulas/aula_funções/index.php
@@ -0,0 +1,10 @@
+<?php 
+    function Saudacao(){
+        return 'boa tarde!';
+    }
+    function ResumirTexto(){
+        return 'texto resumido!';
+    } 
+
+    echo Saudacao(),"<br>", ResumirTexto();
+?>
