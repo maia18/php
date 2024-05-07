6c575213958216edab5c4645748c9914f9631af3
maia18 <nelsonkaue05100@gmail.com>
2024-05-07 14:43:11 -0300

    Update index.php

--- aulas/aula_funções/index.php
+++ aulas/aula_funções/index.php
@@ -1,10 +1,33 @@
 <?php 
-    function Saudacao(){
-        return 'boa tarde!';
+    function Saudacao():string
+    {   
+        date_default_timezone_set("America/Sao_Paulo");
+        $hora = date('H:i:s');
+        
+        if ($hora >= 0 AND $hora <= 5) {
+            $saudação = "Boa madrugada!";
+        } elseif($hora >= 6 && $hora <= 12){
+            $saudação = "Bom dia!";
+        } else if($hora >= 13 AND $hora<=18){
+            $saudação = "Boa tarde!";
+        } else {
+            $saudação = "Boa noite!";
+        }
+        return $saudação;
     }
-    function ResumirTexto(){
-        return 'texto resumido!';
-    } 
 
-    echo Saudacao(),"<br>", ResumirTexto();
+    function ResumirTexto(string $texto, int $limite, $continue = "..."):string
+    {
+        $texto_limpo = trim($texto);
+        if(mb_strlen($texto_limpo) <= $limite){
+            return $texto_limpo;
+        }
+
+        $resumirTexto = mb_substr($texto_limpo, 0, mb_strrpos(mb_substr($texto_limpo, 0, $limite), ''));
+
+        // $sem_tags = strip_tags(); ---> Apaga Tags 
+        return $resumirTexto.$continue;
+    }
+
+    echo Saudacao(),"<br>", ResumirTexto("<strong>Hello, world!</strong>", 10);
 ?>
